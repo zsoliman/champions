5806ba08d8cdd96b7dcb91f44c085436e0819394
Zachary Soliman <zachsoliman1@gmail.com>
2022-07-01 16:42:38 -0400

    image changes when class is selected

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const classSelector = document.getElementById('class-select')
+const charImg = document.getElementById('char-img')
 const baseImgUrl = 'https://www.dndbeyond.com/attachments/thumbnails/0/'
 const portraitMap = {
-    'fighter':
-        'wizard':
-    'barbarian':
-        'warlock':
-    'bard':
-        'cleric':
-    'monk':
-        'paladin':
-    'druid':
-        'ranger':
-    'rogue':
+    'barbarian': '679/400/417/c3barbarianintro.png',
+    'bard': '684/400/406/c3bardintro.png',
+    'cleric': '687/380/437/c3clericintro.png',
+    'druid': '693/400/399/c3druidintro.png',
+    'fighter': '697/400/475/c3fighterintro.png',
+    'paladin': '701/400/473/c3paladinintro.png',
+    'monk': '700/400/490/c3monkintro.png',
+    'ranger': '707/400/444/c3rangerintro.png',
+    'rogue': '709/375/480/c3rogueintro.png',
+    'sorcerer': '712/400/517/c3sorcererintro.png',
+    'wizard': '717/400/484/c3wizardintro.png',
+    'warlock': '716/400/512/c3warlockintro.png'
 }
 
 let request = async () => {
     // initiate request to dnd server
-    let req = await fetch('https://www.dnd5eapi.co/api/classes/')
+    let req = await fetch('https://www.dnd5eapi.co/api/classes')
     // get response from server and convert it to JSON
     let res = await req.json()
     res.results.forEach((char) => {
@@ -24,18 +26,17 @@ let request = async () => {
         option.setAttribute('value', char.name)
         option.innerText = char.name
         classSelector.append(option)
-    });
+    })
 
     classSelector.addEventListener('change', (e) => {
         document.getElementById('selected-class').innerText = e.target.value
-        let img = document.createElementById('img')
-        img.src = `${baseImgUrl}${portraitMap[e.target.value]}`
-        img.setAttribute('width', '400px')
-        img.setAttribute('height', '400px')
-        document.body.append(img)
-
+        let src = `${baseImgUrl}${portraitMap[e.target.value.toLowerCase()]}`
+        charImg.src = src
+        charImg.classList.remove('hidden')
+        console.log(e.target.value)
     })
 }
 
-// the request function will now automatically load
+
+// the request function will now automatically run when the page loads
 request()
